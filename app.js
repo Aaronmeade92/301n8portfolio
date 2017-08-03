02242129b7a8c37292600a590ebef184d453ec0e
Aaron <aaronmeade9292@gmail.com>
2017-08-03 14:44:45 -0700

    figuring out handlebars

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict'
+
+var
+
+function projects(){
+  this.title
+  this.img
+  this.info
+}
+
+$(function () {
+
+  var theTemplateScript = $('#projects-template').html();
+
+  var theTemplate = Handlebars.compile(theTemplateScript);
+
+  
+})
