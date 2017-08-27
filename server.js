2aeebc2ee592f7634383ca395f6ad8a36e5a00ce
Aaron <aaronmeade9292@gmail.com>
2017-08-27 14:30:00 -0700

    added node packages

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const pg = require('pg');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
