fc4051b5168c4c1c63226e2b0f5b4431cd801aaa
Aaron <aaronmeade9292@gmail.com>
2017-09-04 19:34:12 -0700

    added port

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log('This port is ' + PORT);
+})
+app.use(express.static('./public'))
